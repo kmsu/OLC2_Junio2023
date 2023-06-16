428207968102362ffe38aeb0fa64eac6b9e23d2e
Kevin Samayoa <ingkevins@gmail.com>
2023-06-15 22:42:22 -0600

    update

--- backend/src/Instrucciones/ciclo_while.py
+++ backend/src/Instrucciones/ciclo_while.py
@@ -15,10 +15,9 @@ class CWhile(Abstract):
         super().__init__(fila, columna)
     
     def interpretar(self, arbol, tabla):
-        print(str(self.bloqueWhile) + "este es el bloque")
         # a expresion se le asigna un valor (1 , "2" , true)
         nuevaTabla = TablaSimbolos(tabla)  # NUEVO ENTORNO
-        expresion = self.expresion.interpretar(arbol, tabla)
+        expresion = self.expresion.interpretar(arbol, nuevaTabla)
         # si la expresion viene mal, salta excpcion
         if isinstance(expresion, Excepcion): return expresion
         # obtenemos el tipo de la expresion
@@ -28,25 +27,13 @@ class CWhile(Abstract):
 
             # Recorriendo las instrucciones
             #expresion va a ser igual a un interpretar del bloque while -- buscamos en nueva tabla su valor
-            while (expresion):
-                # nuevo_valor = expresion[i]               
-                # simbolo = Simbolo(self.variable.ide, self.variable.tipo, nuevo_valor, self.fila, self.columna)
-                # # Actualizando el valor de la variable en la tabla de simbolos
-                # valor = nuevaTabla.updateTabla(simbolo)
-                # if isinstance(valor, Excepcion): return valor 
-                
+            while (expresion):                
                 for instruccion in self.bloqueWhile:
                     result = instruccion.interpretar(arbol, nuevaTabla)
                     if isinstance(result, Excepcion):
                         arbol.excepciones.append(result)
                 
                 expresion = self.expresion.interpretar(arbol, nuevaTabla)
-                
-                # simbolo = Simbolo(str(self.ide), self.valor.tipo, expresion, self.fila, self.columna)
-                # result = tabla.setTabla(simbolo)
-                # if isinstance(result, Excepcion): return result
-                #gettable (expresion) - esto nos devolvera la fila de la tabla y sacamos valor
-                #expresion = False
                 
             return None
             
